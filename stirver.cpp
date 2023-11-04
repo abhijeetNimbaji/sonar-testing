f0d6edd3a4cd3d74321deee205291b39130e74b6
Abhijeet Nimbaji <63029070+abhijeetNimbaji@users.noreply.github.com>
2023-11-04 18:29:26 +0530

    Add files via upload

---
+++ stirver.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	cout << n;
+	for (int i = 0; i < 2; i++) {
+		cout << i;
+	}
+}
